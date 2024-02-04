204f0ba83c8654be2c7dd997c9dc7c7f6d138b1b
bredsk <nicolas.programador@gmail.com>
2024-02-04 19:52:43 -0300

    m7

--- isapre/settings.py
+++ isapre/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'isapre.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/postgres',
         conn_max_age=600,
         
     )
