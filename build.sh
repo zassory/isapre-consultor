e7f754a9b44cc917ff62e242b96a3909774b9dad
bredsk <nicolas.programador@gmail.com>
2024-02-04 20:57:11 -0300

    mm4.2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('nicolas', 'nicolas.programador@gmail.com', 'Hakxf2n5$')" | python manage.py shell
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('nicolas', 'nicolas.programador@gmail.com', 'Hakxf2n5$')" | python manage.py shell
 
 python manage.py collectstatic --no-input
 python manage.py migrate
