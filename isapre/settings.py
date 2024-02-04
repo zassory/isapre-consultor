ce01fe0c697b9c21b33ec4a3a0a155abb477d29e
bredsk <nicolas.programador@gmail.com>
2024-02-04 19:55:56 -0300

    m8

--- isapre/settings.py
+++ isapre/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+#DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
