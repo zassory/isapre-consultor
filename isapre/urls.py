fec463c99027de8f85ffe0759999c190fde22b23
bredsk <nicolas.programador@gmail.com>
2024-02-04 19:21:16 -0300

    m4

--- isapre/urls.py
+++ isapre/urls.py
@@ -7,8 +7,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include('noticias.urls')),
     path('admin/', admin.site.urls),
-    path('', include('noticias.urls')),
+    path('noticias/', include('noticias.urls')),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
