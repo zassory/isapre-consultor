39d43577f8d911ec8b80dc7d0ba3250417070d00
bredsk <nicolas.programador@gmail.com>
2024-02-04 21:12:15 -0300

    mm4.5

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 # poetry install
 pip install -r requirements.txt
-pip install pillow
 
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('nicolas', 'nicolas.programador@gmail.com', 'Hakxf2n5$')" | python manage.py shell
 
--- isapre/settings.py
+++ isapre/settings.py
@@ -146,7 +146,7 @@ if not DEBUG:
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
