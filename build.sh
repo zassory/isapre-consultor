78a1bec8d166181df11f7e89033c521aca0a7aea
bredsk <nicolas.programador@gmail.com>
2024-02-04 21:05:20 -0300

    mm4.4

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # poetry install
 pip install -r requirements.txt
+pip install pillow
 
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('nicolas', 'nicolas.programador@gmail.com', 'Hakxf2n5$')" | python manage.py shell
 
