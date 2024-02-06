4174ff034783dd19e43ca8f84aea767f0127c19a
bredsk <nicolas.programador@gmail.com>
2024-02-06 20:15:10 -0300

    otro cambio

--- isapre/settings.py
+++ isapre/settings.py
@@ -147,7 +147,7 @@ if not DEBUG:
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
