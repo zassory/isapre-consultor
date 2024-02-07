4be55d88e92cb20840d91d3f4ccb5e8ff7685ef9
bredsk <nicolas.programador@gmail.com>
2024-02-07 18:23:48 -0300

    otro cambio

--- isapre/settings.py
+++ isapre/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'noticias/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
